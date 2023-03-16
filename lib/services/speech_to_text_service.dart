0f89e8c02d7040315ab258ff0ff77d88782502a1
Sajjad Ullah <sajjadullah725@gmail.com>
2023-03-16 20:25:24 +0000

    version 1 with button to get file directory

--- lib/services/speech_to_text_service.dart
+++ lib/services/speech_to_text_service.dart
@@ -18,15 +18,16 @@ class ChatGPTService {
   Future <String> transcribe(File audioFile) async {
     try {
       final endpoint = Uri.parse('https://api.openai.com/v1/audio/transcriptions');
-      final tokenAPI = dotenv.env['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'];
+      const tokenAPI = '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';
       const model = 'whisper-1';
 
       final request = http.MultipartRequest('POST', endpoint)
-        ..headers['Authorization'] = 'Bearer $tokenAPI'
-        ..headers['Content-Type'] = 'multipart/form-data'
+        ..headers['Authorization'] = 'Bearer: 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 '
+        ..headers['Content-Type'] = 'application/json'
+        //..headers['Content-Type'] = 'multipart/form-data'
         ..fields['model'] = model
-        ..files.add( audioFile as http.MultipartFile);
-      //..files.add(await http.MultipartFile.fromPath('file', audioFile.path));
+       // ..files.add( audioFile as http.MultipartFile);
+      ..files.add(await http.MultipartFile.fromPath('file', audioFile.path));
 
       final response = await request.send();
 
@@ -34,13 +35,44 @@ class ChatGPTService {
         final responseString = await response.stream.bytesToString();
         final jsonResponse = json.decode(responseString);
 
-        return jsonResponse['transcription'];
+        return jsonResponse['text'];
       } else {
         return 'Error: ${response.statusCode}';
       }
     } catch (e) {
       print(e);
-      return 'couldnt';
+      return 'could not convert';
     }
   }
+}
+
+
+
+
+
+Future<String> generateResponse(String prompt) async {
+  const apiKey = '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';
+
+  var url = Uri.https("api.openai.com", "/v1/audio/transcriptions");
+  final response = await http.post(
+    url,
+    headers: {
+      'Content-Type': 'application/json',
+      "Authorization": "Bearer $apiKey"
+    },
+    body: json.encode({
+      "model": "text-davinci-003",
+      "prompt": prompt,
+      'temperature': 0,
+      'max_tokens': 2000,
+      'top_p': 1,
+      'frequency_penalty': 0.0,
+      'presence_penalty': 0.0,
+    }),
+  );
+
+  // Do something with the response
+  Map<String, dynamic> newresponse = jsonDecode(response.body);
+
+  return newresponse['choices'][0]['text'];
 }
